6e01290c3746a489abdfd5758f71c3fc0f55c1ba
proplayer919 <pro_player919@outlook.com>
2026-01-14 16:47:25 +1100

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,19 @@ import Register from './pages/auth/register/Register';
 import Game from './pages/game/Game';
 import { applyTheme, initThemeListener } from './helpers/theme';
 import { loadSettings } from './helpers/settings';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   initThemeListener();
   applyTheme(loadSettings().theme);
 
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/auth/login" element={<Login />} />
-        <Route path="/auth/register" element={<Register />} />
-        <Route path="/game" element={<Game />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Landing />} />
+      <Route path="/auth/login" element={<Login />} />
+      <Route path="/auth/register" element={<Register />} />
+      <Route path="/game" element={<Game />} />
+    </Routes>
   );
 }
 
