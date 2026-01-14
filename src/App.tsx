95b59287222030a2bc95bb93ebc4a5f4beb79883
proplayer919 <pro_player919@outlook.com>
2026-01-14 16:52:14 +1100

    fix?

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,21 @@ import Register from './pages/auth/register/Register';
 import Game from './pages/game/Game';
 import { applyTheme, initThemeListener } from './helpers/theme';
 import { loadSettings } from './helpers/settings';
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   initThemeListener();
   applyTheme(loadSettings().theme);
 
   return (
-    <Routes>
-      <Route path="/" element={<Landing />} />
-      <Route path="/auth/login" element={<Login />} />
-      <Route path="/auth/register" element={<Register />} />
-      <Route path="/game" element={<Game />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/auth/login" element={<Login />} />
+        <Route path="/auth/register" element={<Register />} />
+        <Route path="/game" element={<Game />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
