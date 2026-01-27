2ae381eed636f64f7b4045cad23edd72a8b3b11a
proplayer919 <pro_player919@outlook.com>
2026-01-27 12:40:37 +1100

    add

--- server/common/fishing/fishingRods.ts
+++ server/common/fishing/fishingRods.ts
@@ -3,6 +3,15 @@ export interface FishingRodInfo {
   name: string;
   price: number;
   multiplier: number;
+  buyable: boolean;
 }
 
-export const fishingRods: FishingRodInfo[] = [];
+export const fishingRods: FishingRodInfo[] = [
+  {
+    id: 'damaged-rod',
+    name: 'Damaged Rod',
+    price: 0,
+    multiplier: 1,
+    buyable: false,
+  }
+];
